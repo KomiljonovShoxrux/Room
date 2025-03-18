d5ea971b37b63a4afdf14b3a27e776a844fc1c23
Shoxrux <komiljonovshoxrux19@gmail.com>
2025-03-18 11:39:43 +0500

    add project

--- src/main/java/org/example/roomtwo/Controller/LikeController.java
+++ src/main/java/org/example/roomtwo/Controller/LikeController.java
@@ -10,7 +10,8 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@Service
+@RestController
+@RequestMapping("/like")
 public class LikeController {
     @Autowired
     LikeService likeService;
